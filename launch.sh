df51d429ffbf5531379ec3724658f0f794211430
Konstantin <pro100kot14@gmail.com>
2019-12-22 22:40:09 +0300

    add circle package

--- launch.sh
+++ launch.sh
@@ -5,3 +5,4 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
+rosrun circle_drive circle_drive.py
---
+++ packages/circle_drive/scripts/circle_drive.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import rospy
+from duckietown import DTROS
+from duckietown_msgs.msg import Twist2DStamped
+
+class MyNode(DTROS):
+
+    def __init__(self, node_name):
+        super(MyNode, self).__init__(node_name=node_name)
+        self.pub = rospy.Publisher("~/car_cmd", Twist2DStamped, queue_size=1)
+
+    def run(self):
+        # publish message every 1 second
+        rate = rospy.Rate(1) # 1Hz
+        while not rospy.is_shutdown():
+            msg = Twist2DStamped()
+            msg.v = 0.0
+            msg.omega = 0.5
+            rospy.loginfo("Publishing message")
+            self.pub.publish(msg)
+            rate.sleep()
+            msg.omega = -0.5
+            rospy.loginfo("Publishing message -")
+            self.pub.publish(msg)
+            rate.sleep()
+
+if __name__ == '__main__':
+    # create the node
+    node = MyNode(node_name='circle_drive_node')
+    # run node
+    node.run()
+    # keep spinning
+    rospy.spin()
