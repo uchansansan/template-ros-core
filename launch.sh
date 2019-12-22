adfdfc18dd3a28868d39b7d25d6cd6b94a5c7b25
Konstantin <pro100kot14@gmail.com>
2019-12-22 22:46:17 +0300

    add rocdore run

--- launch.sh
+++ launch.sh
@@ -5,4 +5,6 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
+roscore &
+sleep 5
 rosrun circle_drive circle_drive.py
