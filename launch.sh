2a460efddb58021326bb1036443235deb153f69d
Konstantin <pro100kot14@gmail.com>
2019-12-24 12:28:10 +0300

    change launch

--- launch.sh
+++ launch.sh
@@ -5,6 +5,4 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
-roscore &
-sleep 5
-rosrun circle_drive circle_drive.py
+roslaunch circle_drive circle_drive.launch
