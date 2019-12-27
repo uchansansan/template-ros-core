f0d4ea5a856fe0ed24b846610351ddc90f2e1737
Konstantin <pro100kot14@gmail.com>
2019-12-27 17:34:17 +0300

    test master launch

--- launch.sh
+++ launch.sh
@@ -5,4 +5,5 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
-roslaunch circle_drive circle_drive.launch
+#roslaunch circle_drive circle_drive.launch
+roslaunch circle_drive indefinite_navigation.launch
