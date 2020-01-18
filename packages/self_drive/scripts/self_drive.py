b64a7ddb9a7bf734dd1b5c81b8335f1c6485f96e
uchansansan <uchansansan@gmail.com>
2020-01-18 15:34:22 +0700

    , v;sdklgm;klwm k;mefk k fkla

--- packages/self_drive/scripts/self_drive.py
+++ packages/self_drive/scripts/self_drive.py
@@ -9,7 +9,8 @@ import threading
 current_milli_time = lambda: int(round(time.time()*1000))
 
 def RobotListener(msg):
-	rospy.loginfo('%s',str(msg))
+	global controller 
+	rospy.loginfo('MESSAGE s%s',str(msg))
 	controller.run_dyty_cycle(current_milli_time(), RobotInfo(msg.v, msg.omega))
 
 
