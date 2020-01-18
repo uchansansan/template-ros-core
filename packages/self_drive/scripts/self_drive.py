4206e6122289277346f0a2d094d26ba5b6d235b3
uchansansan <uchansansan@gmail.com>
2020-01-18 14:55:09 +0700

    commit 100500

--- packages/self_drive/scripts/self_drive.py
+++ packages/self_drive/scripts/self_drive.py
@@ -1,8 +1,50 @@
 #!/usr/bin/env python
 import rospy
+import cv2 as cv
 from duckietown_msgs.msg import Twist2DStamped
+from Road_map import *
+from GUI import *
+import threading 
+
+current_milli_time = lambda: int(round(time.time()*1000))
+
 def RobotListener(msg):
 	rospy.loginfo('%s',str(msg))
-rospy.init_node('self_drive')
-rospy.Subscriber('/autobot03/car_cmd_switch_node/cmd', Twist2DStamped, RobotListener)
+	controller.run_dyty_cycle(current_milli_time(), RobotInfo(msg.v, msg.omega))
+
+
+class RobotInfo:
+	def __init__(self, v, omega):
+		self.v = v
+		self.omega = omega
+
+class Controller:
+	def __init__(self, read_img):
+
+		self.road_map = Roadmap(read_img,x=0,y=0)
+		self.gui = GUI()
+		self.first = True
+		rospy.init_node('self_drive')
+		self.listener = rospy.Subscriber('/autobot03/car_cmd_switch_node/cmd', Twist2DStamped, RobotListener)
+		self.mutex = threading.Lock()
+
+	def run_dyty_cycle(self, time_stamp, info):
+	 	if self.first == True:
+	 		last_step_info = info
+	 		last_time_stamp = time_stamp
+	 		self. first = False
+	 		return
+	 	self.road_map.setTrace(time_stamp - last_time_stamp, last_step_info.omega, last_step_info.v, info.v)
+	 	last_time_stamp = time_stamp
+	 	last_step_info = info
+		mutex.acquire()
+	 	self.gui.redraw(self.road_map)
+		mutex.release()
+
+
+
+read_img = cv.imread('graph.jpg',1)
+controller = Controller(read_img)
+
+
 rospy.spin()
