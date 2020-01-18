711d34de920f2968295e31f7993932019fac76a6
uchansansan <uchansansan@gmail.com>
2020-01-18 20:42:50 +0700

    qefafe

--- packages/self_drive/scripts/Road_map.py
+++ packages/self_drive/scripts/Road_map.py
@@ -15,65 +15,69 @@ class Roadmap():
 		rospy.loginfo('%s',str("setTrace-ALLAH"))
 		img = self.img
 		x_line1,y_line1,x_line2,y_line2 = self.getTangent()
-		x1= 250
-		y1= 250
+		x1= int(250)
+		y1= int(250)
 		try:
-			alpha_line = math.ceil((y_line1 - y_line2) / (x_line1 - x_line2))
+			alpha_line = int(math.ceil((y_line1 - y_line2) / (x_line1 - x_line2)))
 		except ZeroDivisionError:
-			alpha_line = math.ceil((y_line1 - y_line2))
-		distance1 = math.ceil(last_v)
-		distance2 = math.ceil(v)
+			alpha_line = int(math.ceil((y_line1 - y_line2)))
+		distance1 = int(math.ceil(last_v))
+		distance2 = int(math.ceil(v))
 		if alpha_robot == numpy.pi / 2:
 			if alpha_line == numpy.pi / 2:
-				cv.line(img, (math.ceil(x1), math.ceil(y1)), (math.ceil(x1), math.ceil(y1 + distance1 - distance2)), (255, 0, 0), 2)
-				y1 = math.ceil(y1 + distance1 - distance2)
-				distance1 = math.ceil(distance2)
+				cv.line(img, ( int(math.ceil(x1)), int(math.ceil(y1)) ), ( int(math.ceil(x1)), int(math.ceil(y1 + distance1 - distance2)) ), (255, 0, 0), 2)
+				
+				y1 = int(math.ceil(y1)) + distance1 - distance2
+				
+				distance1 = int(math.ceil(distance2))
 			else:
-				cv.line(img, (math.ceil(x1), math.ceil(y1)), (math.ceil(x1 + distance1 - distance2), math.ceil(y1)), (255, 0, 0), 2)
-				x1 = math.ceil(x1 + distance1 - distance2)
-				distance1 = math.ceil(distance2)
+				cv.line(img, (int(math.ceil(x1)), int(math.ceil(y1))), (int(math.ceil(x1 + distance1 - distance2), int(math.ceil(y1)))), (255, 0, 0), 2)
+				
+				x1= x1 + distance1 - distance2 
+				
+				distance1 = int(math.ceil(distance2))
 		else: 
 			if distance1 == distance2:
 				if alpha_line == numpy.pi / 2:
 					cv.line(img, (x1, y1), (x1 + 20, y1), (255, 0, 0), 2)
-					x1 = math.ceil(x1 + 20)
+					x1 = int(math.ceil(x1 + 20))
 			else:
-				cv.line(img, (math.ceil(x1), math.ceil(y1)), (math.ceil(x1), math.ceil(y1 - 20)), (255, 0, 0), 2, 8)
-				y1 = math.ceil(y1 - 20)
-		if distance1 > distance2 and alpha_robot != numpy.pi / 2 and alpha_robot != 3*numpy.pi / 2:	
+				cv.line(img, ( int(math.ceil(x1)), int(math.ceil(y1)) ), ( int(math.ceil(x1)), int(math.ceil(y1 - 20)) ), (255, 0, 0), 2, 8)
+				y1 = int(math.ceil(y1 - 20))
+		if (distance1 > distance2) and (alpha_robot != numpy.pi / 2) and (alpha_robot != 3*numpy.pi / 2):	
 			if alpha_line == numpy.pi / 2:
-				distance3 = math.ceil((distance1-distance2) / math.sin(alpha_robot))
-				x2 = math.ceil(distance3 * math.cos(alpha_robot))
-				y2 = math.ceil(distance3 * math.sin(alpha1))
-				cv.line(img, (x1, y1), (x1 + round(x2), y1 + round(y2)), (255, 0, 0), 2)
-				x1 = math.ceil(x1 + round(x2))
-				y1 = math.ceil(y1 - round(y2))
-				distance1 = math.ceil(distance2)
+				distance3 = int(math.ceil( (distance1-distance2) / math.sin(alpha_robot) ))
+				x2 = int(math.ceil(distance3 * math.cos(alpha_robot)))
+				y2 = int(math.ceil(distance3 * math.sin(alpha1)))
+				cv.line(img, (int(math.ceil(x1)), int(math.ceil(y1))), (int(math.ceil(x1 + round(x2))), int(math.ceil(y1 + round(y2)))), (255, 0, 0), 2)
+				x1 = int(math.ceil(x1 + round(x2)))
+				y1 = int(math.ceil(y1 - round(y2)))
+				distance1 = int(math.ceil(distance2))
 			else:
-				distance3 = math.ceil((distance1-distance2) / math.sin(alpha_robot))
-				x2 = math.ceil(distance3 * math.sin(alpha_robot))
-				y2 = math.ceil(distance3 * math.cos(alpha_robot))
-				cv.line(img, (x1, y1), (x1 + round(x2), y1 - round(y2)), (255, 0, 0), 2)
-				x1 = math.ceil(x1 + round(x2))
-				y1 = math.ceil(y1 - round(y2))
-				distance1 = math.ceil(distance2)
-		if distance1 < distance2 and alpha_robot != numpy.pi / 2 and alpha_robot != 3*numpy.pi / 2:
-			if alpha_line == numpy.pi / 2 and alpha_robot != numpy.pi / 2:
-				distance3 = math.ceil((distance2-distance1) / math.sin(alpha_robot))
-				x2 = math.ceil(distance3 * math.cos(alpha_robot))
-				y2 = math.ceil(distance3 * math.sin(alpha_robot))
-				cv.line(img, (x1, y1), (x1 + round(x2), y1 - round(y2)), (255, 0, 0), 2)
-				x1 = math.ceil(x1 + round(x2))
-				y1 = math.ceil(y1 - round(y2))
-				distance1 = math.ceil(distance2)
-			elif alpha_robot != numpy.pi / 2 and alpha_robot != 3*numpy.pi / 2:	
-				distance3 = math.ceil((distance2-distance1) / math.sin(alpha_robot))
-				x2 = math.ceil(distance3 * math.sin(alpha_robot))
-				y2 = math.ceil(distance3 * math.cos(alpha_robot))
-				cv.line(img, (math.ceil(x1), math.ceil(y1)), (math.ceil(x1 - round(x2)), math.ceil(y1 - round(y2))), (255, 0, 0), 2)
-				x1 = math.ceil(x1 - round(x2))
-				y1 = math.ceil(y1 - round(y2))
-				distance1 = math.ceil(distance2)
+				distance3 = int(math.ceil( (distance1-distance2) / math.sin(alpha_robot) ) )
+				x2 = int(math.ceil(distance3 * math.sin(alpha_robot)))
+				y2 = int(math.ceil(distance3 * math.cos(alpha_robot)))
+				cv.line(img, (int(math.ceil(x1)), int(math.ceil(y1))), (int(math.ceil(x1 + round(x2))), int(math.ceil(y1 - round(y2)))), (255, 0, 0), 2)
+				x1 = int(math.ceil(x1 + round(x2)))
+				y1 = int(math.ceil(y1 - round(y2)))
+				distance1 = int(math.ceil(distance2))
+		if (distance1 < distance2) and (alpha_robot != numpy.pi / 2) and (alpha_robot != 3*numpy.pi / 2):
+			if (alpha_line == numpy.pi / 2) and (alpha_robot != numpy.pi / 2):
+				distance3 = int(math.ceil( (distance2-distance1) / math.sin(alpha_robot) ))
+				x2 = int(math.ceil(distance3 * math.cos(alpha_robot)))
+				y2 = int(math.ceil(distance3 * math.sin(alpha_robot)))
+				cv.line(img, (int(math.ceil(x1)), int(math.ceil(y1))), ( int(math.ceil(x1 + round(x2))), int(math.ceil(y1 - round(y2))) ), (255, 0, 0), 2)
+				x1 = int(math.ceil(x1 + round(x2)))
+				y1 = int(math.ceil(y1 - round(y2)))
+				distance1 = int(math.ceil(distance2))
+			else if (alpha_robot != numpy.pi / 2) and (alpha_robot != 3*numpy.pi / 2):	
+				distance3 = int(math.ceil((distance2-distance1) / math.sin(alpha_robot)))
+				x2 = int(math.ceil(distance3 * math.sin(alpha_robot)))
+				y2 = int(math.ceil(distance3 * math.cos(alpha_robot)))
+				cv.line(img, ( int(math.ceil(x1)), int(math.ceil(y1)) ), ( int(math.ceil(x1 - round(x2))), int(math.ceil(y1 - round(y2) )) ), (255, 0, 0), 2)
+				x1 = int(math.ceil(x1 - round(x2)))
+				y1 = int(math.ceil(y1 - round(y2)))
+				distance1 = int(math.ceil(distance2))
 
 		#img update
 
