0009eb41062e30fac85d470254f194b3e6e8914d
uchansansan <uchansansan@gmail.com>
2020-01-18 16:36:10 +0700

    sfddsdaf

--- packages/self_drive/scripts/self_drive.py
+++ packages/self_drive/scripts/self_drive.py
@@ -27,19 +27,22 @@ class Controller:
 		self.road_map = Roadmap(read_img,x=0,y=0)
 		self.gui = GUI()
 		self.first = True
-
+		self.last_step_info = RobotInfo(0,0)
+		self.last_time_stamp = 0
 		self.mutex = threading.Lock()
+		rospy.init_node('self_drive')
+l		self.istener = rospy.Subscriber('/autobot03/car_cmd_switch_node/cmd', Twist2DStamped, RobotListener)
 
 	def run_dyty_cycle(self, time_stamp, info):
 		rospy.loginfo('%s',str("dyty_cycle"))
 	 	if self.first == True:
-	 		last_step_info = info
-	 		last_time_stamp = time_stamp
+	 		self.last_step_info = info
+	 		self.last_time_stamp = time_stamp
 	 		self.first = False
 	 		return
-	 	self.road_map.setTrace(time_stamp - last_time_stamp, last_step_info.omega, last_step_info.v, info.v)
-	 	last_time_stamp = time_stamp
-	 	last_step_info = info
+	 	self.road_map.setTrace(time_stamp - self.last_time_stamp, self.last_step_info.omega, self.last_step_info.v, info.v)
+	 	self.last_time_stamp = time_stamp
+	 	self.last_step_info = info
 		mutex.acquire()
 	 	self.gui.redraw(self.road_map)
 		mutex.release()
@@ -48,7 +51,5 @@ class Controller:
 
 read_img = cv.imread('graph.jpg',1)
 controller = Controller(read_img)
-rospy.init_node('self_drive')
-listener = rospy.Subscriber('/autobot03/car_cmd_switch_node/cmd', Twist2DStamped, RobotListener)
 
 rospy.spin()
